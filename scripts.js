f55d516e02b515b15a0e8aee447a51a0d61fc397
Elwin <eshen1998@gmail.com>
2024-07-03 00:21:06 -0600

    Added slideshow

--- scripts.js
+++ scripts.js
@@ -1,21 +1,99 @@
-const projectsButton = document.querySelector(".js-projects");
-console.log(projectsButton)
-projectsButton.addEventListener("click", hello)
-
-function hello() {
-    window.alert("hello world")
-}
-
 document.addEventListener("DOMContentLoaded", setupNav)
 
-function setupNAV() {
-    const navbarLinks = document.querySelectorAll(".navbar a");
-    const sections = document.querySelectorAll("anchor");
-    window.addEventListener("scroll", setupScroll);
+let slideIndex = 0;
+//showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slideshow-image");
+  console.log(slides)
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
 
-function setupScroll() {
-    const currentPos = window.scrollY;
+function setupNav() {
+    const navSection = document.querySelectorAll("nav .section");
 
-    sections.forEach();
+    // click event
+    navSection.forEach(v => {
+        v.onclick = (() => {
+                setTimeout(() => {
+                    navSection.forEach(j => j.classList.remove("active"));
+                    v.classList.add("active");
+            }, 300);
+        });
+    });
+
+    const sections = document.querySelectorAll(".anchor");
+    const menu_links = document.querySelectorAll("nav .section");
+
+    const makeActive = (link) => menu_links[link].classList.add("active");
+    const removeActive = (link) => menu_links[link].classList.remove("active");
+    const removeAllActive = () => [...Array(sections.length).keys()].forEach((link) => removeActive(link));
+
+    const sectionMargin = 200;
+    let currentActive = 0;
+
+    window.addEventListener("scroll", () => {
+        const current = sections.length - [...sections].findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin ) - 1
+      
+        if (current !== currentActive) {
+          removeAllActive();
+          currentActive = current;
+          makeActive(current);
+        }
+      });
+    /*
+    (function() {
+        'use strict';
+      
+        var section = document.querySelectorAll(".anchor");
+        var sections = {};
+        var i = 0;
+      
+        Array.prototype.forEach.call(section, function(e) {
+          sections[e.id] = e.offsetTop;
+        });
+      
+        window.onscroll = function() {
+            console.log("fuck me")
+            var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+      
+            for (i in sections) {
+                if (sections[i] <= scrollPosition) {
+                document.querySelector('.active').setAttribute('class', ' ');
+                document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+            }
+          }
+        };
+      })();
+      */
+
+    // scroll event
+
+    /*
+    window.onscroll = (() => {
+        let bodySection = document.querySelectorAll("body .anchor");
+
+        bodySection.forEach((v, i) => {
+            let rect = v.getBoundingClientRect().y;
+            console.log(rect);
+            console.log("window"+ window.innerHeight);
+            if (rect < window.innerHeight - 500) {
+                navSection.forEach(v => v.classList.remove("active"));
+                navSection[(3-i)].classList.add("active");
+            }
+        })
+    })
+    */
 }
+
+/*
+            console.log("rect:"+ rect);
+            console.log("window:" + window.innerHeight);
+*/
